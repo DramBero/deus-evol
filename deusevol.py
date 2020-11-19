cbb64b2c11713c47f64be6773616c2c5e443730a
DramBero <60761907+DramBero@users.noreply.github.com>
2020-11-19 19:19:03 +0300

    Add files via upload
    Added image sources for pixelorama and uploaded new file version with map generation, shadows, freckles, etc

--- deusevol.py
+++ deusevol.py
@@ -1,4 +1,5 @@
 import pygame
+from itertools import product
 import math
 import numpy as np
 
@@ -20,6 +21,16 @@ parameters:
     
 '''
 
+def tile_prepare(image, color, flip=False):
+    tile = pygame.image.load(image)
+    if flip:
+        tile = pygame.transform.flip(tile, True, False)
+    tile = pygame.transform.scale(tile, (cellsize, cellsize))
+    colortile = pygame.Surface(tile.get_size()).convert_alpha()
+    colortile.fill(color)
+    tile.blit(colortile, (0,0), special_flags = pygame.BLEND_RGBA_MULT)
+    return tile
+
 # Создаем игру и окно
 pygame.init()
 pygame.mixer.init()
@@ -34,7 +45,7 @@ hairb = ['graphics/hairback.png']
 hairunique = ('graphics/hairbald.png')
 clothesm = ['graphics/bikinim.png']
 clothesf = ['graphics/bikinif.png']
-cellsize = 32
+cellsize = 48
 
 def truncated_normal(mean, stddev, minval, maxval):
     return np.clip(np.random.normal(mean, stddev), minval, maxval)
@@ -76,7 +87,7 @@ class Player(pygame.sprite.Sprite):
 
 class Beast(pygame.sprite.Sprite):
     def __init__(self, x, y, gmelanine, gskincrange, gskinbalance, \
-                 female, gmuscularity, gspeed, ghairred):
+                 female, gmuscularity, gspeed, ghairred, ghairlength):
         pygame.sprite.Sprite.__init__(self)
         self.married = False
         self.dead = False
@@ -84,9 +95,14 @@ class Beast(pygame.sprite.Sprite):
         self.gmuscularity = gmuscularity
         self.gspeed = gspeed
         self.muscularity = gmuscularity - (female * 0.3 * gmuscularity) 
-        self.image = pygame.image.load('graphics/head.png')
+        self.image = pygame.Surface((16, 16))
+        self.image = pygame.image.load('graphics/shadow.png')
+        self.head = pygame.image.load('graphics/head.png')
+        self.image.blit(self.head, (0, 0))
         self.image = pygame.transform.scale(self.image, (cellsize, cellsize))
         self.width = 0
+        self.hairlength = ghairlength
+        self.hairgreyness = 0
         self.hairback = 0
         if not self.female:
             self.body = pygame.image.load('graphics/bodym.png')
@@ -102,8 +118,9 @@ class Beast(pygame.sprite.Sprite):
         else:
             self.body = pygame.image.load('graphics/bodyf.png')
             self.body = pygame.transform.scale(self.body, (cellsize, cellsize))
+            backlength = int(self.hairlength * (cellsize * (10/16)))
             self.hairback = pygame.image.load(hairb[0])
-            self.hairback = pygame.transform.scale(self.hairback, (cellsize, cellsize))
+            self.hairback = pygame.transform.scale(self.hairback, (int(cellsize * (9/16)), backlength))
             self.hair = pygame.image.load(hairf[np.random.randint(0, len(hairf))])
             self.hair = pygame.transform.scale(self.hair, (cellsize, cellsize))
         self.image.blit(self.body, (self.width/2, 0))
@@ -122,16 +139,28 @@ class Beast(pygame.sprite.Sprite):
                     (1 - self.gskinbalance))
         self.colorImage.fill((red, green, blue))
         self.image.blit(self.colorImage, (0,0), special_flags = pygame.BLEND_RGBA_MULT)
-        red, green, blue = 30, 0, 30
+        red, green, blue = 30, 30, 30
         red += int(self.ghairlight * 225)
-        green = int((red/2) + (red/2) * (1 - self.ghairred))
+        green += int(((red - 30)/2) + ((red - 30)/2) * (1 - self.ghairred))
+        if red > 210 and self.ghairred > 0.5:
+            self.frek = tile_prepare('graphics/freckles.png', (red, green, blue))
+            self.frek.set_alpha(255 * (ghairred - 0.5) * 2)
+            self.image.blit(self.frek, (0, 0))
+        if self.hairgreyness < 0.5:
+            green = green + ((red - green) * (self.hairgreyness * 2))
+            blue = blue + ((red - blue) * (self.hairgreyness * 2))
+        else:
+            red = red + ((255 - red) * ((self.hairgreyness - 0.5) * 2))
+            green = red
+            blue = green
         if self.hairback:
             self.colorhairback = pygame.Surface(self.hairback.get_size()).convert_alpha()
             self.colorhairback.fill((red, green, blue))
             self.hairback.blit(self.colorhairback, (0,0), special_flags = pygame.BLEND_RGBA_MULT)
-            self.image.blit(self.hairback, (0, 0))
+            self.imagec = self.image.copy()
+            self.image.blit(self.hairback, (cellsize * (4/16), cellsize * (4/16)))
+            self.image.blit(self.imagec, (0, 0))
         self.colorhair = pygame.Surface(self.hair.get_size()).convert_alpha()
-        print(red, green, blue)
         self.colorhair.fill((red, green, blue))
         self.hair.blit(self.colorhair, (0,0), special_flags = pygame.BLEND_RGBA_MULT)
         self.image.blit(self.hair, (0, 0))
@@ -145,10 +174,6 @@ class Beast(pygame.sprite.Sprite):
         self.rect.y = y
         self.speedx = 0
         self.speedy = 0
-        if not self.female:
-            self.body_wear(clothesm[0], (120, 190, 0))
-        else:
-            self.body_wear(clothesf[0], (120, 190, 0))
 
     def update(self, x=0, y=0):
         self.speedx = x
@@ -183,12 +208,10 @@ class Beast(pygame.sprite.Sprite):
         if not self.female and not self.married:
             prefered = []
             for beast in beasts:
-                if not beast.female:
-                    continue
                 if beast.female and not beast.married:
                     prefered.append(beast)
             while len(prefered) > 1:
-                if prefered[0].attract > prefered[1].attract:
+                if abs(prefered[0].attract - self.attract) < abs(prefered[1].attract - self.attract):
                     prefered.remove(prefered[1])
                 else:
                     prefered.remove(prefered[0])
@@ -230,6 +253,8 @@ class Beast(pygame.sprite.Sprite):
         if self.female:
             self.married = True
             beast.married = True
+            self.body_wear(clothesf[0], (120, 190, 0))
+            beast.body_wear(clothesm[0], (120, 190, 0))
             chance = np.random.random()
             self.givebirth(beast)
             if chance > 0.99:
@@ -257,9 +282,10 @@ class Beast(pygame.sprite.Sprite):
             (self.gspeed + beast.gspeed)/2, 0.05, 0.001, 0.999)
         ghairred = truncated_normal(\
             (self.ghairred + beast.ghairred)/2, 0.05, 0.001, 0.999)
+        ghairlength = np.random.random()
         child = Beast(x, y, gmelanine, gskincrange, \
                             gskinbalance, female, gmuscularity, gspeed, \
-                            ghairred)
+                            ghairred, ghairlength)
         beasts.append(child)
         all_sprites.add(child)
         
@@ -267,7 +293,6 @@ class Beast(pygame.sprite.Sprite):
         beasts.remove(self)
         self.speed = 0
         self.dead = True
-        print('dead')
         self.colorImage.fill((255, 255, 255))
         self.image.blit(self.colorImage, (0,0), special_flags = pygame.BLEND_RGBA_MULT)
 
@@ -284,7 +309,7 @@ all_sprites = pygame.sprite.Group()
 
 beast_coords = []
 beasts = []
-while len(beast_coords) < 20:
+while len(beast_coords) < 40:
     x_coord = np.random.randint(0, int(width/cellsize)) * cellsize
     y_coord = np.random.randint(0, int(height/cellsize)) * cellsize
     if (x_coord, y_coord) not in beast_coords:
@@ -292,10 +317,10 @@ while len(beast_coords) < 20:
         beasts.append(Beast(x_coord, y_coord, np.random.random(), \
                             np.random.random(), np.random.random(), \
                             np.random.choice((0, 1)), np.random.random(), \
-                            np.random.random(), np.random.random()))
+                            np.random.random(), np.random.random(), \
+                            np.random.random()))
         
 player = Player()
-print(beast_coords[0][0], beast_coords[0][1])
 
 #beast1 = Beast(beast_coords[0][0], beast_coords[0][1])
 
@@ -303,12 +328,36 @@ print(beast_coords[0][0], beast_coords[0][1])
 for i in beasts:
     all_sprites.add(i)
 
+
+map1 = np.ones((40, 40), dtype=int)
+map2 = np.random.randint(1, 10, size=(40, 40))
+print(map2)
+
+
+def setup_background():
+    screen.fill((0, 0, 0))
+    plain = tile_prepare('graphics/grass.png', (150, 180, 50))
+    plains = [0, plain]
+    plant1 = tile_prepare('graphics/plants1.png', (140, 200, 50))
+    plant1f = tile_prepare('graphics/plants1.png', (140, 200, 50), flip=True)
+    plant2 = tile_prepare('graphics/plants2.png', (140, 200, 50))
+    plant2f = tile_prepare('graphics/plants2.png', (140, 200, 50), flip=True)
+    decs = [0, 0, 0, 0, 0, 0, plant1, plant1f, plant2, plant2f]
+    tile_width, tile_height = plain.get_width(), plain.get_height()
+    for x,y in product(range(0,tile_width * len(map1[0]), tile_width),
+                                 range(0,tile_height * len(map1[0]),tile_height)):
+        screen.blit(plains[map1[int(x/tile_width)][int(y/tile_height)]], (x, y))
+        if decs[map2[int(x/tile_width)][int(y/tile_height)]] != 0:
+            screen.blit(decs[map2[int(x/tile_width)][int(y/tile_height)]], (x, y))
+#        screen.blit(np.random.choice([plant1, plant1f, plant2, plant2f]), (x, y))
+
 # Цикл игры
 running = True
 while running:
     # Держим цикл на правильной скорости
     clock.tick(FPS)
     # Ввод процесса (события)
+    
     for event in pygame.event.get():
         # проверка для закрытия окна
         if event.type == pygame.QUIT:
@@ -316,9 +365,8 @@ while running:
 
     # Обновление
     all_sprites.update()
-    
+    setup_background()
     # Рендеринг
-    screen.fill((150, 180, 50))
     all_sprites.draw(screen)
     # После отрисовки всего, переворачиваем экран
     pygame.display.flip()
